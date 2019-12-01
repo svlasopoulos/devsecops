b6950e23b0af96a1fa394984307eea7c5f7f4477
svlasopoulos <svlasopoulos@gmail.com>
2019-12-01 14:52:42 +0100

    sonarqube scanner

--- sonar-project.js
+++ sonar-project.js
@@ -3,7 +3,7 @@ const sonarqubeScanner = require('sonarqube-scanner');
        // serverUrl : is the SonarQube Url that matches appserver machine ip address and port 9000 where SonarQube server runs
        serverUrl: 'http://192.168.56.103:9000',  
        options : {
-       'sonar.sources': '.',
+       'sonar.sources': '.'
        //'sonar.inclusions' : '.' // Entry point of your code
        }
      }, () => {});
